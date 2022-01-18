3df7ac3fa27099f6c29b3fe39f694ecc0eec0870
Shubham12121212 <96530712+Shubham12121212@users.noreply.github.com>
2022-01-19 05:06:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+if (window.performance.navigation.type === 1) {
+  rollDice();
+}
+function rollDice(){
+var x = (Math.round(Math.random()*10))%6 + 1;
+var y = "images/dice"+x+".png";
+var z = (Math.round(Math.random()*10))%6 + 1;
+var w = "images/dice"+z+".png";
+if(x>z){
+  document.querySelector("h1").innerHTML =" Player 1 winner ⛳";
+}
+if(z>x){
+  document.querySelector("h1").innerHTML =" Player 2 winner ⛳";
+}
+if(z===x){
+  document.querySelector("h1").innerHTML =" Game Draw 🥱🥱";
+}
+document.querySelector(".img1").setAttribute("src",y);
+document.querySelector(".img2").setAttribute("src",w);}
